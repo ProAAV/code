aba4e5a7247c92f89160b26a393f9ba3f68fe01b
hcc123231 <2028915986@qq.com>
2025-02-18 23:51:45 +0800

    2025-2-18 hcc 23:51

--- qtCode/ProAVV/aav_networkmanager.cpp
+++ qtCode/ProAVV/aav_networkmanager.cpp
@@ -4,6 +4,7 @@
 #include<QHttpMultiPart>
 #include<QFile>
 #include<QFileInfo>
+#include<QMimeDatabase>
 #include<QEventLoop>
 NetWorkManager::NetWorkManager(QObject *parent): QObject{parent}
 {
@@ -38,7 +39,8 @@ void NetWorkManager::http_upload_file(const QString& file_path,const QString &ur
 
     // 创建一个文件部分
     QHttpPart filePart;
-    filePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"file\"; filename=\"" + QFileInfo(file_path).fileName() + "\""));
+    filePart.setHeader(QNetworkRequest::ContentDispositionHeader,
+                       QVariant("form-data; name=\"file\"; filename=\"" + QFileInfo(file_path).fileName() + "\""));
     QFile *file = new QFile(file_path);
     if (!file->open(QIODevice::ReadOnly)) {
         qDebug() << "无法打开文件：" << file_path;
@@ -50,11 +52,24 @@ void NetWorkManager::http_upload_file(const QString& file_path,const QString &ur
     // 将文件部分添加到多部分对象中
     multiPart->append(filePart);
 
+    // 获取文件的 MIME 类型
+    QMimeDatabase mimeDatabase;
+    QMimeType mimeType = mimeDatabase.mimeTypeForFile(file_path);
+    QString contentType = mimeType.isValid() ? mimeType.name() : "application/octet-stream";
+
+    // 创建一个 `file_content_type` 字段部分
+    QHttpPart contentTypePart;
+    contentTypePart.setHeader(QNetworkRequest::ContentDispositionHeader, QVariant("form-data; name=\"file_content_type\""));
+    contentTypePart.setBody(contentType.toUtf8());
+
+    // 将 `file_content_type` 字段添加到多部分对象中
+    multiPart->append(contentTypePart);
+
     // 创建请求
     QNetworkRequest request(url);
 
     // 设置 Content-Type 头为 multipart/form-data
-    request.setHeader(QNetworkRequest::ContentTypeHeader, "multipart/form-data;boundary=" + boundary);
+    request.setHeader(QNetworkRequest::ContentTypeHeader, "multipart/form-data; boundary=" + boundary);
 
     // 发起 POST 请求
     QNetworkReply *reply = networkManager.post(request, multiPart);
@@ -62,14 +77,14 @@ void NetWorkManager::http_upload_file(const QString& file_path,const QString &ur
 
     // 使用事件循环等待响应
     QEventLoop loop;
-    connect(reply, &QNetworkReply::finished,&loop,&QEventLoop::quit);
+    connect(reply, &QNetworkReply::finished, &loop, &QEventLoop::quit);
     loop.exec();
 
     // 处理响应
     if (reply->error()) {
         qDebug() << "上传失败：" << reply->errorString();
     } else {
-    qDebug() << "上传成功，服务器响应：" << reply->readAll();
+        qDebug() << "上传成功，服务器响应：" << reply->readAll();
     }
 
     reply->deleteLater();
